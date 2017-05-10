cfeac9a385a1bf752ef996196c9eef004905ecdf
Patrick Hussey <husspm@gmail.com>
2017-05-10 17:51:19 -0400

    server up and running, a few routes, and a decoy table for testing

---
+++ app/routes/homepageRoutes.js
@@ -0,0 +1,40 @@
+var database = require("../models");
+console.log("CONTROLLERS pass");
+module.exports = function(app) {
+
+    app.get("/", function(req, res) {
+        res.render("home");
+    });
+
+    app.get("/users", function(req, res) {
+        res.render("users");
+    });
+
+    app.get("/users/:username", function(req, res) {
+        database.Client.findOne({
+            where: { username: req.params.username }
+        }).then(function(data) {
+            res.render("clients", { contents: data });
+        });
+    });
+    app.put("/users/:username", function(req, res) {
+        database.Client.update({
+            name: req.body.name,
+            address: req.body.address,
+            contact: req.body.phone
+        }, {
+            where: { username: req.params.username }
+        }).then(function() {
+            res.redirect("/users/" + req.params.username);
+        });
+    });
+    app.post("/users", function(req, res) {
+        database.Client.create({
+            email: req.body.email,
+            password: req.body.password,
+            username: req.body.user
+        }).then(function(data) {
+            res.redirect("/users/" + data.username);
+        });
+    });
+}; //ends exports function
